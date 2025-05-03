1013cab7acd22b79a93938a09cb10a859a61b211
trishna <trishnabhattarai36@gmail.com>
2025-05-03 16:14:23 +0545

    changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
 import ChatLayout from './components/ChatLayout';
 import { ChatProvider } from './context/ChatContext';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <ChatProvider>
-        <ChatLayout />
-      </ChatProvider>
-    </div>
+    <ChatProvider>
+      <ChatLayout />
+    </ChatProvider>
   );
 }
 
